83cb0adcad5cab7de247ee7263a1fcb375a111d0
Javier Fernández <leqjl93@hotmail.com>
2024-02-09 17:27:32 -0600

    x

--- src/js/aboutViewer.vue.js
+++ src/js/aboutViewer.vue.js
@@ -27,19 +27,31 @@ const AboutViewer = {
                 </div>
             </div>
             <div class="py-5">
-                <div class="h2 text-success">Somos</div>
-                <div class="lead lead-xl">Somos la fusión armoniosa entre la antigua sabiduría maya y la moderna innovación. En cada diseño, llevamos la esencia de la naturaleza y la rica cultura de Yucatán. Nuestro compromiso con la calidad y la sostenibilidad se refleja en cada detalle, creando no solo productos, sino experiencias que cuentan la historia de nuestra conexión con la tierra y sus tradiciones.</div>
-                <img src="../../src/img/about.png" alt="about" title="about" class="w-100 rounded mt-5"/>
-            </div>
-            <div class="py-5">
-                <div class="h2 text-success">Visión</div>
-                <div class="lead lead-xl">Somos la fusión armoniosa entre la antigua sabiduría maya y la moderna innovación. En cada diseño, llevamos la esencia de la naturaleza y la rica cultura de Yucatán. Nuestro compromiso con la calidad y la sostenibilidad se refleja en cada detalle, creando no solo productos, sino experiencias que cuentan la historia de nuestra conexión con la tierra y sus tradiciones.</div>
-                <img src="../../src/img/about.png" alt="about" title="about" class="w-100 rounded mt-5"/>
-            </div>
-            <div class="py-5">
-                <div class="h2 text-success">Nuestra Comunidad</div>
-                <div class="lead lead-xl">Somos la fusión armoniosa entre la antigua sabiduría maya y la moderna innovación. En cada diseño, llevamos la esencia de la naturaleza y la rica cultura de Yucatán. Nuestro compromiso con la calidad y la sostenibilidad se refleja en cada detalle, creando no solo productos, sino experiencias que cuentan la historia de nuestra conexión con la tierra y sus tradiciones.</div>
-                <img src="../../src/img/about.png" alt="about" title="about" class="w-100 rounded mt-5"/>
+                <div class="row gx-6 justify-content-center">
+                    <div class="col-12 col-md">
+                        <div class="h2 text-success">Misión</div>
+                        <div class="lead lead-xl">Nuestra misión en Universo de Jade es proporcionar a nuestros clientes oportunidades de inversión seguras y rentables en desarrollos sustentables. Nos comprometemos a ofrecer un servicio de alta calidad, basado en la transparencia y cumplimiento de las normas legales, garantizando la satisfacción y tranquilidad de nuestros clientes. Creemos en la construcción de relaciones a largo plazo, basadas en la confianza y el respeto mutuo.</div>
+                        <img src="../../src/img/mision.jpg" alt="about" title="about" class="w-100 rounded mt-5"/>
+                    </div>
+                    <div class="col-12 col-md">
+                        <div class="h2 text-success">Visión</div>
+                        <div class="lead lead-xl">En Universo de Jade, aspiramos a ser reconocidos a nivel nacional e internacional como líderes en la venta de Bienes Raíces con un concepto de sustentabilidad y conexión. Queremos ser conocidos por nuestro compromiso con el medio ambiente, la transparencia y la satisfacción del cliente. Nuestra visión es ser un referente en la industria, marcando la pauta en prácticas comerciales éticas y sostenibles.</div>
+                        <img src="../../src/img/vision.jpg" alt="about" title="about" class="w-100 rounded mt-5"/>
+                    </div>
+                </div>
+                <div class="row g-6 py-5 justify-content-center">
+                    <div class="col-12 col-md">
+                        <div class="h2 text-success">Valores</div>
+                        <div class="">
+                            <div class="lead lead-xl">1. ⁠Sustentabilidad:Creemos en el cuidado del medio ambiente y promovemos prácticas que minimicen nuestro impacto en el planeta.</div>
+                            <div class="lead lead-xl">2.⁠ ⁠Transparencia:Nos regimos por un alto nivel de honestidad en todas nuestras operaciones y comunicaciones. Trabajamos para garantizar que nuestros clientes estén bien informados y seguros en cada decisión de inversión.</div>
+                            <div class="lead lead-xl">3.⁠ ⁠Respeto:Valoramos la diversidad y promovemos el respeto y la inclusión en todas nuestras interacciones.</div>
+                            <div class="lead lead-xl">4.⁠ ⁠Responsabilidad:Asumimos la responsabilidad de nuestras acciones y decisiones y nos esforzamos por cumplir con nuestras promesas.</div>
+                            <div class="lead lead-xl">5.⁠ ⁠Innovación:Estamos constantemente buscando formas de mejorar y ofrecer soluciones creativas a nuestros clientes.</div>
+                        </div>
+                        <img src="../../src/img/valores.jpg" alt="about" title="about" class="w-100 rounded mt-5"/>
+                    </div>
+                </div>
             </div>
         </div>
     `
