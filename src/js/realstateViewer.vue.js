faddbc976d482867eaad73f3067f8450143123a2
Javier Fernández <leqjl93@hotmail.com>
2024-02-14 13:21:50 -0600

    Update realstateViewer.vue.js

--- src/js/realstateViewer.vue.js
+++ src/js/realstateViewer.vue.js
@@ -27,8 +27,10 @@ const RealstateViewer = {
         <div v-if="realStates" class="container py-6">
             <div class="row g-5 py-5">
                 <div v-for="(realState,index) in realStates" class="col-12 col-md-4">
-                    <div class="card cursor-pointer z-zoom-element animation-fall-down card-real card-body" :style="{'background-image':'url('+realState.image+')','--delay':(index*250)+'ms'}">
-                    </div>
+                    <a :href="realState.link">
+                        <div class="card cursor-pointer z-zoom-element animation-fall-down card-real card-body" :style="{'background-image':'url('+realState.image+')','--delay':(index*250)+'ms'}">
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
