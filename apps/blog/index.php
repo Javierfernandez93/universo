14807eb50d4e88178515d65792d7525820402c7b
Javier Fernández <leqjl93@hotmail.com>
2024-02-23 09:09:20 -0600

    Update index.php

--- apps/blog/index.php
+++ apps/blog/index.php
@@ -2,16 +2,15 @@
 
 require_once TO_ROOT . "/system/core.php";
 
-if($UserSupport->logged === false) {
-	HCStudio\Util::redirectTo('../../apps/admin-login/');
+if(!isset($_GET['bid']))
+{
+	HCStudio\Util::redirectTo(TO_ROOT."/apps/blog/list");
 }
 
-if($UserSupport->hasPermission('list_blog') === false) {
-	HCStudio\Util::redirectTo('../../apps/admin/invalid_permission');
-}
+$Layout = JFStudio\Layout::getInstance();
 
 $route = JFStudio\Router::BLOG;
-$Layout = JFStudio\Layout::getInstance();
+
 $Layout->init(JFStudio\Router::getName($route),'index',"blog",'',TO_ROOT.'/');
 
 $Layout->setScript([
