8e7066bd7fd4d828567105774ceb142b44584792
Javier Fernández <leqjl93@hotmail.com>
2024-02-29 17:23:53 -0600

    Update getConfigVarsStats.php

--- app/application/getConfigVarsStats.php
+++ app/application/getConfigVarsStats.php
@@ -7,6 +7,7 @@ $data = HCStudio\Util::getHeadersForWebService();
 if(true)
 {
     $data["stats"] = [
+        'company_email' => Site\SystemVar::_getValue("company_email"),
         'social_whatsapp' => Site\SystemVar::_getValue("social_whatsapp"),
         'experience' => Site\SystemVar::_getValue("experience"),
         'real_state' => Site\SystemVar::_getValue("real_state"),
