feebc017ea84c66d6f1cc725fd394f039ce34fda
javierfernandez93 <javier.fernandez.pa93@gmail.com>
2024-07-01 10:16:19 -0600

    fix: email template

--- apps/mail/view/welcome-academy.view.php
+++ apps/mail/view/welcome-academy.view.php
@@ -1,6 +1,9 @@
 <h2>¡Hola! <b><?php echo $names; ?></b></h2>
 
-<h3>Bienvenido a la Academia de <?php echo Site\SystemVar::_getValue("company_name");?></h3>
+<h3>¡Es un placer darte la bienvenida a la Academia de Universo de Jade! Nos complace informarte que tu acceso ha sido habilitado y ahora puedes comenzar a disfrutar de todos nuestros recursos educativos y herramientas de aprendizaje.</h3>
+<h3>En la Academia de Universo de Jade, nos comprometemos a proporcionarte una experiencia educativa de alta calidad que te permitirá expandir tus conocimientos en bienes raices. Aquí encontrarás cursos diseñados por expertos, materiales interactivos y la oportunidad de conectar con una comunidad de aprendizaje apasionada.</h3>
+<h3>Para empezar, te recomendamos explorar nuestro catálogo de cursos y recursos disponibles en tu área de interés. No dudes en contactarnos si tienes alguna pregunta o necesitas asistencia adicional. Estamos aquí para ayudarte en cada paso de tu camino hacia el éxito.</h3>
+<h3>¡Una vez más, bienvenido/a a la comunidad de Universo de Jade! Estamos emocionados de tener la oportunidad de apoyarte en tu viaje de aprendizaje.</h3>
 
 <h3>
     <b>
